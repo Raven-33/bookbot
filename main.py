c480e071212cace8f2c2258e20cb83c93eed63ff
Raven-33 <cfrocha15@gmail.com>
2025-08-04 20:29:55 +0100

    Suffered to do sort and report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import num_words
 from stats import count_char
+from stats import list_d
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -10,8 +11,24 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     words = num_words(book_text)
     char = count_char(book_text)
-    print (f"{words} words found in the document")
-    print(char)
+    sort = []
+    for i,e in char.items():
+        sort.append({"char":i,"num":e})
+    sort.sort(reverse=True,key=list_d)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt")
+    print("----------- Word Count ----------")
+    print(f"Found {words} total words")
+    print("--------- Character Count -------")
+    for i in sort:
+       character = i["char"]
+       count = i["num"]
+       if character.isalpha():
+           print(f"{character}: {count}")
+
+    print("============= END ===============")
+  
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,9 +1,9 @@
-def num_words (book_text):
+def num_words(book_text):
     word_l = book_text.split()
     words = len(word_l)
     return words
 
-def count_char (book_text):
+def count_char(book_text):
     count = {}
     characters = book_text.lower()
     
@@ -12,4 +12,14 @@ def count_char (book_text):
             count[f"{i}"] = 0
         count[i] += 1
     return count
-    
+
+
+def list_d(sort):
+    return sort["num"]
+
+
+
+
+
+
+
